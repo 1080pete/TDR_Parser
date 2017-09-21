b25bcb0c310dc0f63405debdf46d2ee790a6c693
ajreno95 <ajreno952@gmail.com>
2017-09-21 13:59:06 -0400

    framed main.py

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
+#create global variable for count
+#create global dict? read in as id then change based on func
+
+
+
+lex(){
+}
+match(){
+}
+prog(){
+}
+letInEnd(){
+}
+declList(){
+}
+decl(){
+}
+type(){
+}
+expr(){
+}
+term(){
+}
+factor(){
+}
+
+factor(){
+	lex();
+	if(nextToken=="id"){
+	}
+	elif
+}
+
+
 main(){
 
-}	
+}
+
+
+
+
+	
